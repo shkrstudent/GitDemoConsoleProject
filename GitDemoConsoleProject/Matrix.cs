103ff5f29ebc8e49dba47c31a1368b9cd8c96d34
Kristina Sheremet <zipzm211_shkr@student.ztu.edu.ua>
2022-06-06 23:00:33 +0100

    Deleting of display method of class matrix

--- GitDemoConsoleProject/Matrix.cs
+++ GitDemoConsoleProject/Matrix.cs
@@ -20,16 +20,6 @@ namespace GitDemoConsoleProject
             return matrix;
 
         }
-        public static void Display(int[,] matrix)
-        {
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                    Console.Write($"{matrix[i, j]}\t");
-                }
-                Console.WriteLine();
-            }
-        }
+        
     }
 }
