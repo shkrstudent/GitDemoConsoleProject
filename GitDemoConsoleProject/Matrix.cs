cfe19f1f7cdb310d6176442a769e89a2a17e9df3
Kristina Sheremet <zipzm211_shkr@student.ztu.edu.ua>
2022-06-06 20:11:49 +0100

    Created method of displaying matrix

--- GitDemoConsoleProject/Matrix.cs
+++ GitDemoConsoleProject/Matrix.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace GitDemoConsoleProject
 {
-    internal class Matrix
+    public class Matrix
     {
+        public static int[,] Generate(int n, int m)
+        { 
+            Random random = new Random();
+            int[,] matrix = new int[n, m];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i, j] = random.Next(0, 100);
+                }
+            return matrix;
+
+        }
+        public static void Display (int [,]matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+
     }
 }
